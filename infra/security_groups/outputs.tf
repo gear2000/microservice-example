639133fa5af28f993c6d11e2a3ef548ed6edcd0b
Gary <gary@elasticdev.io>
2020-02-25 17:08:21 +0800

    update

--- infra/security_groups/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "internal_id" {
-  value = "${aws_security_group.internal.id}"
-}
-
-output "public_id" {
-  value = "${aws_security_group.public.id}"
-}
--- infra/security_groups/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "vpc_id" {}
-variable "vpc_region" {}
-variable "sg_internal" {}
-variable "sg_public" {}
-variable "subnet_public_cidr" {}
