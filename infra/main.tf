04c8539bc19dae6180a6d401c296c27280b411c1
Gary <gary@elasticdev.io>
2020-02-25 19:01:37 +0800

    update

--- infra/main.tf
+++ infra/main.tf
@@ -22,24 +22,71 @@ module "vpc" {
   }
 }
 
-module "ecs_cluster" {
-  source = "infrablocks/ecs-cluster/aws"
-  version = "0.2.5"
-  
-  region  = "us-east-1"
-  vpc_id = "${module.vpc.vpc_id}"
-  subnet_ids = "${module.vpc.private_subnets[0]}"
-  
-  component = "important-component"
-  deployment_identifier = "production"
-  
-  cluster_name = "services"
-  cluster_instance_ssh_public_key_path = "~/.ssh/id_rsa.pub"
-  cluster_instance_type = "t2.small"
-  
-  cluster_minimum_size = 2
-  cluster_maximum_size = 10
-  cluster_desired_capacity = 4
+resource "aws_security_group" "web" {
+	name = "web"
+	description = "Web Security Group"
+
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}		
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
 }
 
-#module.vpc.vpc_cidr_block
+resource "aws_alb" "ecs-load-balancer" {
+    name                = "ecs-load-balancer"
+    security_groups     = ["${aws_security_group.web.id}"]
+    subnet_ids          = ["${module.vpc.public_subnets[0]}", "${module.vpc.public_subnets[1]}" ]
+
+    tags = {
+      Name = "ecs-load-balancer"
+    }
+}
+
+resource "aws_alb_target_group" "ecs-target-group" {
+    name                = "ecs-target-group"
+    port                = "80"
+    protocol            = "HTTP"
+    vpc_id              = "${module.vpc.vpc_id}"
+
+    health_check {
+        healthy_threshold   = "5"
+        unhealthy_threshold = "2"
+        interval            = "30"
+        matcher             = "200"
+        path                = "/"
+        port                = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = "5"
+    }
+
+    tags = {
+      Name = "ecs-target-group"
+    }
+}
+
+resource "aws_alb_listener" "alb-listener" {
+    load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
+    port              = "80"
+    protocol          = "HTTP"
+
+    default_action {
+        target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
+        type             = "forward"
+    }
+}
