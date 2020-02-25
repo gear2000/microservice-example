45fb46c617bff9d2e928f7da64c9ea30979a3453
Gary <gary@elasticdev.io>
2020-02-25 19:06:38 +0800

    update

--- infra/main.tf
+++ infra/main.tf
@@ -48,45 +48,45 @@ resource "aws_security_group" "web" {
 	}
 }
 
-resource "aws_alb" "ecs-load-balancer" {
-    name                = "ecs-load-balancer"
-    security_groups     = ["${aws_security_group.web.id}"]
-    subnet_ids          = "${module.vpc.public_subnets"
-
-    tags = {
-      Name = "ecs-load-balancer"
-    }
-}
-
-#resource "aws_alb_target_group" "ecs-target-group" {
-#    name                = "ecs-target-group"
-#    port                = "80"
-#    protocol            = "HTTP"
-#    vpc_id              = "${module.vpc.vpc_id}"
-#
-#    health_check {
-#        healthy_threshold   = "5"
-#        unhealthy_threshold = "2"
-#        interval            = "30"
-#        matcher             = "200"
-#        path                = "/"
-#        port                = "traffic-port"
-#        protocol            = "HTTP"
-#        timeout             = "5"
-#    }
-#
-#    tags = {
-#      Name = "ecs-target-group"
-#    }
-#}
-#
-#resource "aws_alb_listener" "alb-listener" {
-#    load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
-#    port              = "80"
-#    protocol          = "HTTP"
-#
-#    default_action {
-#        target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
-#        type             = "forward"
-#    }
-#}
+##resource "aws_alb" "ecs-load-balancer" {
+##    name                = "ecs-load-balancer"
+##    security_groups     = ["${aws_security_group.web.id}"]
+##    subnet_ids          = "${module.vpc.public_subnets"
+##
+##    tags = {
+##      Name = "ecs-load-balancer"
+##    }
+##}
+##
+##resource "aws_alb_target_group" "ecs-target-group" {
+##    name                = "ecs-target-group"
+##    port                = "80"
+##    protocol            = "HTTP"
+##    vpc_id              = "${module.vpc.vpc_id}"
+##
+##    health_check {
+##        healthy_threshold   = "5"
+##        unhealthy_threshold = "2"
+##        interval            = "30"
+##        matcher             = "200"
+##        path                = "/"
+##        port                = "traffic-port"
+##        protocol            = "HTTP"
+##        timeout             = "5"
+##    }
+##
+##    tags = {
+##      Name = "ecs-target-group"
+##    }
+##}
+##
+##resource "aws_alb_listener" "alb-listener" {
+##    load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
+##    port              = "80"
+##    protocol          = "HTTP"
+##
+##    default_action {
+##        target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
+##        type             = "forward"
+##    }
+##}
