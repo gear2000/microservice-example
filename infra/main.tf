130ba134adb6a6103160268771203a9a0b7a46fa
Gary <gary@elasticdev.io>
2020-02-25 18:34:11 +0800

    update

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
  profile    = "default"
- region     = var.region
+ region  = "us-east-1"
 }
 
 module "vpc" {
@@ -9,7 +9,7 @@ module "vpc" {
   name = "main-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = var.azs
+  azs             = ["us-east-1a", "us-east-1b" ]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
@@ -26,7 +26,7 @@ module "ecs_cluster" {
   source = "infrablocks/ecs-cluster/aws"
   version = "0.2.5"
   
-  region  = var.region
+  region  = "us-east-1"
   vpc_id = "${module.vpc.vpc_id}"
   subnet_ids = "${module.vpc.private_subnets[0]}"
   
