f22dac6feaf458c64b69c093774227d5b916493d
Gary <gary@elasticdev.io>
2020-02-25 19:27:47 +0800

    update

--- infra/main.tf
+++ infra/main.tf
@@ -25,7 +25,7 @@ module "vpc" {
 resource "aws_security_group" "web" {
 	name = "web"
 	description = "Web Security Group"
-    vpc_id = "$module.vpc.vpc_id"
+    vpc_id = "$module.vpc.id"
 
 	ingress {
 		from_port = 80
@@ -49,41 +49,41 @@ resource "aws_security_group" "web" {
 	}
 }
 
-resource "aws_alb" "ecs-load-balancer" {
-  name                = "ecs-load-balancer"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["$aws_security_group.web.id"]
-  subnets            = ["$module.vpc.public_subnets[0]","$module.vpc.public_subnets[1]"]
-
-  enable_deletion_protection = true
-
-  tags = {
-    Name = "ecs-load-balancer"
-  }
-}
-
-resource "aws_alb_target_group" "ecs-target-group" {
-    name                = "ecs-target-group"
-    port                = "80"
-    protocol            = "HTTP"
-    vpc_id              = "${module.vpc.vpc_id}"
-
-    health_check {
-        healthy_threshold   = "5"
-        unhealthy_threshold = "2"
-        interval            = "30"
-        matcher             = "200"
-        path                = "/"
-        port                = "traffic-port"
-        protocol            = "HTTP"
-        timeout             = "5"
-    }
-
-    tags = {
-      Name = "ecs-target-group"
-    }
-}
+#resource "aws_alb" "ecs-load-balancer" {
+#  name                = "ecs-load-balancer"
+#  internal           = false
+#  load_balancer_type = "application"
+#  security_groups    = ["$aws_security_group.web.id"]
+#  subnets            = ["$module.vpc.public_subnets[0]","$module.vpc.public_subnets[1]"]
+#
+#  enable_deletion_protection = true
+#
+#  tags = {
+#    Name = "ecs-load-balancer"
+#  }
+#}
+#
+#resource "aws_alb_target_group" "ecs-target-group" {
+#    name                = "ecs-target-group"
+#    port                = "80"
+#    protocol            = "HTTP"
+#    vpc_id              = "${module.vpc.vpc_id}"
+#
+#    health_check {
+#        healthy_threshold   = "5"
+#        unhealthy_threshold = "2"
+#        interval            = "30"
+#        matcher             = "200"
+#        path                = "/"
+#        port                = "traffic-port"
+#        protocol            = "HTTP"
+#        timeout             = "5"
+#    }
+#
+#    tags = {
+#      Name = "ecs-target-group"
+#    }
+#}
 
 ##resource "aws_alb_listener" "alb-listener" {
 ##    load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
