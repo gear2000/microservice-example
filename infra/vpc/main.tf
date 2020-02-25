2ff735a4e5282691ec391f86f171bf6a18b83ca0
Gary <gary@elasticdev.io>
2020-02-25 17:05:41 +0800

    update

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -1,8 +1,9 @@
-# Define a vpc
 resource "aws_vpc" "primary_vpc" {
   cidr_block = "${var.vpc_cidr_block}"
-
-  tags {
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
     Name = "${var.vpc_name}"
   }
 }
+
