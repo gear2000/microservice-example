abb1a29a169b29ab3f4711f496b78ed84c3bd37a
Gary <gary@elasticdev.io>
2020-02-26 08:49:10 +0800

    update

--- infra/main.tf
+++ infra/main.tf
@@ -27,49 +27,9 @@ module "web_sg" {
   vpc_id = module.vpc.vpc_id
 }
 
-#resource "aws_alb" "ecs-load-balancer" {
-#  name                = "ecs-load-balancer"
-#  internal           = false
-#  load_balancer_type = "application"
-#  security_groups    = ["${module.security_groups.sg_id}"]
-#  subnets            = ["${module.vpc.public_subnets[0]}","${module.vpc.public_subnets[1]}"]
-#
-#  enable_deletion_protection = true
-#
-#  tags = {
-#    Name = "ecs-load-balancer"
-#  }
-#}
-#
-#resource "aws_alb_target_group" "ecs-target-group" {
-#    name                = "ecs-target-group"
-#    port                = "80"
-#    protocol            = "HTTP"
-#    vpc_id              = "${module.vpc.vpc_id}"
-#
-#    health_check {
-#        healthy_threshold   = "5"
-#        unhealthy_threshold = "2"
-#        interval            = "30"
-#        matcher             = "200"
-#        path                = "/"
-#        port                = "traffic-port"
-#        protocol            = "HTTP"
-#        timeout             = "5"
-#    }
-#
-#    tags = {
-#      Name = "ecs-target-group"
-#    }
-#}
-#
-#resource "aws_alb_listener" "alb-listener" {
-#    load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
-#    port              = "80"
-#    protocol          = "HTTP"
-#
-#    default_action {
-#        target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
-#        type             = "forward"
-#    }
-#}
+module "web_alb" {
+  source = "./modules/web_alb"
+  vpc_id = module.vpc.vpc_id
+  security_groups    = ["${module.web_sg.sg_id}"]
+  subnets            = ["${module.vpc.public_subnets[0]}","${module.vpc.public_subnets[1]}"]
+}
---
+++ infra/modules/alb/outputs.tf
@@ -0,0 +1,3 @@
+output "alb_id" {
+  value = "${aws_alb.ecs-load-balancer.id}"
+}
