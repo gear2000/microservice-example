7e5e166157d665d467f4639117c08f440b743de9
Gary <gary@elasticdev.io>
2020-02-26 16:37:22 +0800

    update

--- infra/modules/iam_roles/main.tf
+++ infra/modules/iam_roles/main.tf
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "ecs-instance-policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
-    role       = "${aws_iam_role.ecs-instance-role.name}"
+    role       = aws_iam_role.ecs-instance-role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
