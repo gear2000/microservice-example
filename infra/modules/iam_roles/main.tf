0b2cb83de717394313dbdd57a9459c871fe6a8fe
Gary <gary@elasticdev.io>
2020-02-26 16:35:06 +0800

    update

--- infra/modules/iam_roles/main.tf
+++ infra/modules/iam_roles/main.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "ecs-service-policy" {
 resource "aws_iam_role" "ecs-service-role" {
     name                = "ecs-service-role"
     path                = "/"
-    assume_role_policy  = "${data.aws_iam_policy_document.ecs-service-policy.json}"
+    assume_role_policy  = data.aws_iam_policy_document.ecs-service-policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs-service-role-attachment" {
