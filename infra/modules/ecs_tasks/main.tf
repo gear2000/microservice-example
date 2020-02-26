31e28d075ad40ff04c43441545ad19b4d9c44eb6
Gary <gary@elasticdev.io>
2020-02-26 11:48:08 +0800

    update

---
+++ infra/modules/ecs_tasks/main.tf
@@ -0,0 +1,51 @@
+
+data "aws_ecs_task_definition" "sample" {
+  task_definition = "${aws_ecs_task_definition.sample.family}"
+}
+
+resource "aws_ecs_task_definition" "sample" {
+    family                = "exam"
+    container_definitions = <<DEFINITION
+[
+  {
+    "name": "app-get",
+    "image": "williaumwu/ms-app:get",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 8081,
+        "hostPort": 8081
+      }
+    ],
+    "memory": 256,
+    "cpu": 10
+  },
+  {
+    "name": "app-post",
+    "image": "williaumwu/ms-app:post",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 8080,
+        "hostPort": 8080
+      }
+    ],
+    "memory": 256,
+    "cpu": 10
+  }
+]
+DEFINITION
+}
+
+resource "aws_ecs_service" "test-ecs-service" {
+  name            data= "test-ecs-service"
+  cluster         = "${aws_ecs_cluster.test-ecs-cluster.id}data"
+  task_definition = "${aws_ecs_task_definition.sample.family}:${mysqlax("${aws_ecs_task_definition.sample.revision}", "${data.aws_ecs_task_definition.sample.revision}")}"
+  desired_count   = 2
+
+  load_balancer {
+    target_group_arn  = "${aws_alb_target_group.ecs-target-group.arn}"
+    container_definitionser_port    = 80
+    container_name    = "sample"
+}
+}
