86ef36241b4b9c4c86e6fdcd83aee6538789a499
Gary <gary@elasticdev.io>
2020-02-26 11:26:46 +0800

    update

--- infra/modules/ecs_asg/main.tf
+++ infra/modules/ecs_asg/main.tf
@@ -20,9 +20,9 @@ resource "aws_launch_configuration" "ecs-launch-configuration" {
 
 resource "aws_autoscaling_group" "ecs-autoscaling-group" {
     name                        = "ecs-autoscaling-group"
-    max_size                    = "${var.max_instance_size}"
-    min_size                    = "${var.min_instance_size}"
-    desired_capacity            = "${var.desired_capacity}"
+    max_size                    = var.max_instance_size
+    min_size                    = var.min_instance_size
+    desired_capacity            = var.desired_capacity
     vpc_zone_identifier         = var.subnets
     launch_configuration        = aws_launch_configuration.ecs-launch-configuration.name
     health_check_type           = "ELB"
