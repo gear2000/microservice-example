e5e75db675adf51b4af0780b62755f362d6c871e
Gary <gary@elasticdev.io>
2020-02-27 13:23:48 +0800

    update

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,32 @@ provider "aws" {
   region  = var.aws_region
 }
 
+# get ECS AMI with owner being "AWS"
+data "aws_ami" "ecs" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["*amazon-ecs-optimized"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["591542846629"] 
+}
+
+# ECS cluster info
+data "template_file" "ecs-cluster" {
+  template = file("ecs-cluster.tpl")
+
+  vars = {
+    ecs_cluster = aws_ecs_cluster.ad.name
+  }
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -59,3 +85,4 @@ module "ecs_tasks" {
   #target_group_arn = aws_alb_target_group.app.id
   iam_role          = "${module.iam_roles.iam_service_role_arn}"
 }
+
