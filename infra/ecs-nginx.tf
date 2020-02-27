df36253ad8d432e519eeedb4b93d6954e7bd7bd7
Gary <gary@elasticdev.io>
2020-02-27 10:21:49 +0800

    update

--- infra/ecs-nginx.tf
+++ infra/ecs-nginx.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_service" "ad-app" {
   depends_on      = [aws_iam_role_policy_attachment.ecs-service-attach]
 
   load_balancer {
-    target_group_arn = aws_alb_target_group.ad-app.id
+    target_group_arn = aws_alb_target_group.add-target-group.id
     container_name   = "ad-app"
     container_port   = "80"
   }
