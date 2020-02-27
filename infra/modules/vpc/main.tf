c7ce0e53b9bfa7fc1223f2fe2c1c02da7863c93b
Gary <gary@elasticdev.io>
2020-02-27 19:14:36 +0800

    update

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -55,8 +55,34 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.vpc.id
 }
 
+## route tables
+#resource "aws_route_table" "public" {
+#  vpc_id = aws_vpc.vpc.id
+#
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = aws_internet_gateway.gw.id
+#  }
+#}
+#
+## route associations public
+#resource "aws_route_table_association" "public-1-a" {
+#  subnet_id      = aws_subnet.public-1.id
+#  route_table_id = aws_route_table.public.id
+#}
+#
+#resource "aws_route_table_association" "public-2-a" {
+#  subnet_id      = aws_subnet.public-2.id
+#  route_table_id = aws_route_table.public.id
+#}
+#
+#resource "aws_route_table_association" "public-3-a" {
+#  subnet_id      = aws_subnet.public-3.id
+#  route_table_id = aws_route_table.public.id
+#}
+
 # route tables
-resource "aws_route_table" "public" {
+resource "aws_route_table" "demo-public" {
   vpc_id = aws_vpc.vpc.id
 
   route {
@@ -66,17 +92,18 @@ resource "aws_route_table" "public" {
 }
 
 # route associations public
-resource "aws_route_table_association" "public-1-a" {
+resource "aws_route_table_association" "demo-public-1-a" {
   subnet_id      = aws_subnet.public-1.id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.demo-public.id
 }
 
-resource "aws_route_table_association" "public-2-a" {
+resource "aws_route_table_association" "demo-public-2-a" {
   subnet_id      = aws_subnet.public-2.id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.demo-public.id
 }
 
-resource "aws_route_table_association" "public-3-a" {
+resource "aws_route_table_association" "demo-public-3-a" {
   subnet_id      = aws_subnet.public-3.id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.demo-public.id
 }
+
